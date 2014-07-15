a2803546838d19a66e605c9e05f06cb6d42abd22
Carlo <carlo@phpbbitalia.net>
2014-07-15 22:51:18 +0200

    Fix coding style

--- pagination.php
+++ pagination.php
@@ -15,7 +15,7 @@ namespace phpbbseo\usu;
 * www.phpBB-SEO.com
 * @package Ultimate SEO URL phpBB SEO
 */
-class pagination extends \phpbb\pagination 
+class pagination extends \phpbb\pagination
 {
 	/** @var \phpbb\template\template */
 	protected $template;
@@ -47,7 +47,7 @@ class pagination extends \phpbb\pagination
 		$this->helper = $helper;
 		$this->php_ext = $php_ext;
 	}
-	
+
 	/**
 	* paginate
 	*
@@ -55,6 +55,7 @@ class pagination extends \phpbb\pagination
 	public function generate_page_link($base_url, $on_page, $start_name, $per_page)
 	{
 		static $paginated = array();
+
 		if (!is_string($base_url))
 		{
 			if (is_array($base_url['routes']))
@@ -65,6 +66,7 @@ class pagination extends \phpbb\pagination
 			{
 				$route = $base_url['routes'];
 			}
+
 			$params = (isset($base_url['params'])) ? $base_url['params'] : array();
 			$is_amp = (isset($base_url['is_amp'])) ? $base_url['is_amp'] : true;
 			$session_id = (isset($base_url['session_id'])) ? $base_url['session_id'] : false;
@@ -76,16 +78,18 @@ class pagination extends \phpbb\pagination
 
 			return $this->helper->route($route, $params, $is_amp, $session_id);
 		}
+
 		if (!isset($paginated[$base_url]))
 		{
 			$rewriten = core::url_rewrite($base_url);
 			@list($rewriten, $qs) = explode('?', $rewriten, 2);
+
 			if (
 				// rewriten urls are absolute
 				!preg_match('`^(https?\:)?//`i', $rewriten) ||
 				// they are not php scripts
 				preg_match('`\.' . $this->php_ext . '$`i', $rewriten)
-			) 
+			)
 			{
 				// in such case, rewrite as usual
 				$url_delim = (strpos($base_url, '?') === false) ? '?' : ((strpos($base_url, '?') === strlen($base_url) - 1) ? '' : '&amp;');
@@ -94,6 +98,7 @@ class pagination extends \phpbb\pagination
 			else
 			{
 				$hasExt = preg_match('`^((https?\:)?//[^/]+.+)(\.[a-z0-9]+)$`i', $rewriten);
+
 				if ($hasExt)
 				{
 					// start location is before the ext
@@ -104,9 +109,11 @@ class pagination extends \phpbb\pagination
 					// start is appened
 					$rewriten = rtrim($rewriten, '/') . '/' . core::$seo_static['pagination'] .  '%2$s' . core::$seo_ext['pagination'];
 				}
+
 				$paginated[$base_url] = $rewriten . ($qs ? "?$qs" : '');
 			}
 		}
+
 		// we'll see if start_name has use cases, and we can still work with rewriterules
 		return ($on_page > 1) ? sprintf($paginated[$base_url], $start_name, ($on_page - 1) * $per_page) : $base_url;
 	}
