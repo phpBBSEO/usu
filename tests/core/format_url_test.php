0fe6f6b156e3470bac8063a884129cd401660ed7
Carlo <carlo@phpbbitalia.net>
2014-07-16 16:36:36 +0200

    format_url test

---
+++ tests/core/format_url_test.php
@@ -0,0 +1,45 @@
+<?php
+/**
+*
+* @package Ultimate SEO URL phpBB SEO
+* @copyright (c) 2006 - 2014 www.phpbb-seo.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbseo\usu\tests\core;
+
+class format_url_test extends \phpbb_test_case
+{
+	public function format_url_test_data()
+	{
+		return array(
+			array(
+				'case'		=> 'This is a test',
+				'expected'	=> 'this-is-a-test',
+			),
+			array(
+				'case'		=> 'phpBB & SEO',
+				'expected'	=> 'phpbb-seo',
+			),
+			array(
+				'case'		=> '&.-',
+				'expected'	=> 'topic',
+			),
+			array(
+				'case'		=> '&.-',
+				'expected'	=> 'forum',
+				'type'		=> 'forum',
+			),
+		);
+	}
+	/**
+	* @dataProvider format_url_test_data
+	*/
+	function test_format_url($case, $expected, $type = 'topic')
+	{
+		$phpbb_seo = new \phpbbseo\usu\core();
+
+		$this->assertEquals($expected, $phpbb_seo->format_url($case), $type);
+	}
+}
