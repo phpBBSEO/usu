cea4b35acec5f34e1c8d0c6361f46e51bee316d5
Carlo <carlo@phpbbitalia.net>
2014-07-23 08:06:00 +0200

    [feature/tests] Add setConfig() for inject specific configuration

--- tests/phpbb_seo_test_case.php
+++ tests/phpbb_seo_test_case.php
@@ -34,4 +34,39 @@ class phpbb_seo_test_case extends \phpbb_test_case
 
 		$this->phpbb_seo = new \phpbbseo\usu\core($config, $request, $user, $auth, $phpbb_root_path, $phpEx, false);
 	}
+
+	protected function setConfig($settings = array(), $forum_urls = array())
+	{
+		$this->phpbb_seo->seo_opt = array(
+			'url_rewrite'			=> false,
+			'modrtype'				=> 3,
+			'sql_rewrite'			=> false,
+			'profile_inj'			=> false,
+			'profile_vfolder'		=> false,
+			'profile_noids'			=> false,
+			'rewrite_usermsg'		=> false,
+			'rewrite_files'			=> false,
+			'rem_sid'				=> false,
+			'rem_hilit'				=> true,
+			'rem_small_words'		=> false,
+			'virtual_folder'		=> false,
+			'virtual_root'			=> false,
+			'cache_layer'			=> true,
+			'rem_ids'				=> false,
+			'redirect_404_forum'	=> false,
+		);
+
+		$this->phpbb_seo->cache_config['forum_urls'] = array();
+
+		$this->phpbb_seo->cache_config['settings'] = & $settings;
+		$this->phpbb_seo->cache_config['forum_urls'] = & $forum_urls;
+		$this->phpbb_seo->cache_config['cached'] = true;
+		$this->phpbb_seo->seo_opt = array_replace_recursive($this->phpbb_seo->seo_opt, $settings);
+		$this->phpbb_seo->modrtype = @isset($this->phpbb_seo->seo_opt['modrtype']) ? $this->phpbb_seo->seo_opt['modrtype'] : $this->phpbb_seo->modrtype;
+
+		if ($this->phpbb_seo->modrtype > 1)
+		{
+			$this->seo_url['forum'] = & $this->cache_config['forum_urls'];
+		}
+	}
 }
